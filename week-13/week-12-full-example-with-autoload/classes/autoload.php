1d8540dc72a02403c0a5a4c0b07d7f28f91b7191
Pachara Tinamas <mrpachara@gmail.com>
2020-10-20 11:26:12 +0700

    autoload with __DIR__

--- week-13/week-12-full-example-with-autoload/classes/autoload.php
+++ week-13/week-12-full-example-with-autoload/classes/autoload.php
@@ -1,5 +1,5 @@
 <?php
 spl_autoload_register(function ($class_name) {
   // If all classes store in the same directory
-  include $class_name . '.php';
+  require __DIR__ . '/'. $class_name . '.php';
 });
